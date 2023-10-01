4b3d2592cbeff97a3221b77222e6d16741700e35
Alena <jujojaa0505@gmail.com>
2023-10-01 20:39:09 +0300

    edited 4th task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,14 +1,9 @@
-// Сначала написала так, но подумала что слишком длинная строка и читать будет неудобно:
-//   function showSalary(users, age) {
-//   return users.reduce((str, user, index) => user.age > age ? str : index === 0 ? `${user.name}, ${user.balance}` : str + '\n' + user.name + ', ' + user.balance, '');
-//   }
+
 function showSalary(users, age) {
-  return users.reduce((str, user, index) => {
-    if (user.age > age) {
-      return str;
-    } else if (index === 0) {
-      return `${user.name}, ${user.balance}`;
-    } 
-      return str + '\n' + user.name + ', ' + user.balance;
-  }, '')
+  return users
+  .filter(user => user.age <= age)
+  .map(user => {
+    return [user.name, user.balance].join(', ')
+  })
+  .join('\n');
 }
