c14fd899d49628635798f5b923db3c2db2d86a5b
Alena <jujojaa0505@gmail.com>
2023-10-05 20:48:53 +0300

    carousel function

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,32 @@
 function initCarousel() {
-  // ваш код...
+  let position = 0;
+  let slideWidth = document.querySelector(".carousel__slide").offsetWidth;
+
+  let left = document.querySelector(".carousel__arrow_left");
+  let right = document.querySelector(".carousel__arrow_right");
+
+  left.style.display = 'none';
+
+
+  right.onclick = function() {
+    position -= slideWidth;
+    left.style.display = '';
+
+    if (position <= -3 * slideWidth) {
+      right.style.display = 'none';
+  }
+
+    document.querySelector(".carousel__inner").style.transform = `translateX(${position}px)`;
+  }
+
+
+
+  left.onclick = function() {
+    position += slideWidth;
+    right.style.display = '';
+
+    if (position == 0) left.style.display = 'none';
+
+    document.querySelector(".carousel__inner").style.transform = `translateX(${position}px)`;
+    }
 }
